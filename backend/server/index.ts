8b8e788965426741ed9838a4255509052d8c7f08
MarcS <marcsalaver@gmail.com>
2025-07-29 14:18:19 -0700

    resolving error in server file

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -1,13 +1,22 @@
 // Entry point for the express server
 import express from "express";
 import routes from "./routes";
+import path from "path";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 
+const frontendDistPath = path.join(__dirname, "../../dist");
+
+app.use(express.static(frontendDistPath));
+
 app.use("/api", routes);
+
+app.get("/*splat", (req, res) => {
+  res.sendFile(path.join(frontendDistPath, "index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
