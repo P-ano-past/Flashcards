bccd4210423b350bc45479f2b45962deb16c777d
Marc Salaver <marcsalaver@gmail.com>
2025-07-31 17:17:51 -0700

    removing usercontext for now until I have db data ready to go

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,13 @@ import { AlertProvider } from "./Context/AlertContext.tsx";
 import "./index.css";
 import App from "./App.tsx";
 import { HeadProvider } from "./Context/HeadContext.tsx";
-import { AuthProvider } from "./Context/UserAuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <AuthProvider>
-      <AlertProvider>
-        <HeadProvider>
-          <App />
-        </HeadProvider>
-      </AlertProvider>
-    </AuthProvider>
+    <AlertProvider>
+      <HeadProvider>
+        <App />
+      </HeadProvider>
+    </AlertProvider>
   </StrictMode>
 );
